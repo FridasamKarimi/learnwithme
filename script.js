4cd9ac5ebd721194a055adfb52cac7fd6374b228
FridasamKarimi <muriithifridasam@gmail.com>
2025-06-20 04:24:27 +0300

    new updates

--- script.js
+++ script.js
@@ -18,3 +18,23 @@ console.log(appleJuice);
 //reuse function
 const appleOrangeJuice = fruitProcessor(3,4);
 console.log(appleOrangeJuice);
+
+
+//convert string into a number
+const num = Number('23');
+console.log(num);
+
+
+//function calculate age - functon declaration and function expression
+function calcAge1(birthYear) {
+    const age = 2037 - birthYear;
+    return age;
+}
+const age1 = calcAge1(2002);
+console.log(age1);
+
+const calcAge2 = function(birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(2005);
+console.log(age1,age2);
